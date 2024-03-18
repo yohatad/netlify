8e8caf2d7e33f1f7fe0026d5a04cc1ed73593229
Yohannes <yohatad000@gmail.com>
2024-03-18 15:21:33 +0200

    New

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 window.onload = function() {
     var nameForm = document.getElementById('nameForm');
     var nameInput = document.getElementById('nameInput');
+    var incomingTextContainer = document.getElementById('incomingText');
+    var receiveMessageButton = document.getElementById('receiveMessage');
     var yesButton = document.getElementById('yesButton');
     var noButton = document.getElementById('noButton');
     var userName = '';
 
     nameForm.onsubmit = function(event) {
-        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+        event.preventDefault();
         userName = nameInput.value.trim();
-        if (userName) {
-            alert('Hello, ' + userName + '! Please make your choice.');
-        } else {
-            alert('Please enter your name.');
-        }
+        console.log('Name submitted:', userName);
     };
+
+    function fetchMessage() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://fit-frank-stork.ngrok-free.app/fetch-message', true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    try {
+                        var response = JSON.parse(xhr.responseText);
+                        incomingTextContainer.innerHTML = '<p>' + response.text + '</p>';
+                    } catch (e) {
+                        alert("Error parsing JSON: " + e);
+                    }
+                }
+            }
+        };
+        xhr.send(null);
+    }
 
     function sendResponse(response) {
         if (userName) {
+            console.log(`Sending response: ${response}`);
             var xhr = new XMLHttpRequest();
             xhr.open('POST', 'https://fit-frank-stork.ngrok-free.app/submit', true);
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    var json = JSON.parse(xhr.responseText);
-                    console.log('Success:', json);
+                    console.log('POST response received');
                 }
             };
             var data = JSON.stringify({ name: userName, response: response });
             xhr.send(data);
-            alert(userName + ' clicked ' + response);
         } else {
-            alert('Please enter your name first.');
+            console.log('No name entered.');
         }
     }
 
+    receiveMessageButton.onclick = fetchMessage;
     yesButton.onclick = function() { sendResponse('Yes'); };
     noButton.onclick = function() { sendResponse('No'); };
 };
