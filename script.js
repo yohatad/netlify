8632e28161d8c6c24ddf3661d94c17035cd4269d
Yohannes <yohatad000@gmail.com>
2024-03-18 15:25:03 +0200

    Revert "New"
    This reverts commit 8e8caf2d7e33f1f7fe0026d5a04cc1ed73593229.

--- script.js
+++ script.js
@@ -1,55 +1,39 @@
 window.onload = function() {
     var nameForm = document.getElementById('nameForm');
     var nameInput = document.getElementById('nameInput');
-    var incomingTextContainer = document.getElementById('incomingText');
-    var receiveMessageButton = document.getElementById('receiveMessage');
     var yesButton = document.getElementById('yesButton');
     var noButton = document.getElementById('noButton');
     var userName = '';
 
     nameForm.onsubmit = function(event) {
-        event.preventDefault();
+        event.preventDefault ? event.preventDefault() : (event.returnValue = false);
         userName = nameInput.value.trim();
-        console.log('Name submitted:', userName);
+        if (userName) {
+            alert('Hello, ' + userName + '! Please make your choice.');
+        } else {
+            alert('Please enter your name.');
+        }
     };
-
-    function fetchMessage() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://fit-frank-stork.ngrok-free.app/fetch-message', true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    try {
-                        var response = JSON.parse(xhr.responseText);
-                        incomingTextContainer.innerHTML = '<p>' + response.text + '</p>';
-                    } catch (e) {
-                        alert("Error parsing JSON: " + e);
-                    }
-                }
-            }
-        };
-        xhr.send(null);
-    }
 
     function sendResponse(response) {
         if (userName) {
-            console.log(`Sending response: ${response}`);
             var xhr = new XMLHttpRequest();
             xhr.open('POST', 'https://fit-frank-stork.ngrok-free.app/submit', true);
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4 && xhr.status === 200) {
-                    console.log('POST response received');
+                    var json = JSON.parse(xhr.responseText);
+                    console.log('Success:', json);
                 }
             };
             var data = JSON.stringify({ name: userName, response: response });
             xhr.send(data);
+            alert(userName + ' clicked ' + response);
         } else {
-            console.log('No name entered.');
+            alert('Please enter your name first.');
         }
     }
 
-    receiveMessageButton.onclick = fetchMessage;
     yesButton.onclick = function() { sendResponse('Yes'); };
     noButton.onclick = function() { sendResponse('No'); };
 };
